773341aed6188da1c9906e67cd63b2510d10fcfc
GitHub Action <action@github.com>
2022-01-23 20:12:27 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/hypomania/mockgrpcresponse/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.hypomania.mockgrpcresponse.services
+
+import com.github.hypomania.mockgrpcresponse.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
